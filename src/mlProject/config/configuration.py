b35f99409f1d58c44c631aed781ad3e1a4c6c714
ajaychaudhary8955 <ajaychaudhary8104@gmail.com>
2026-02-05 12:41:25 +0530

    add data validation training pipeline

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,5 +1,6 @@
 from src.mlProject.constants import *
 from src.mlProject.entity.config_entity import DataIngestionConfig
+from src.mlProject.entity.config_entity import DataValidationConfig
 from src.mlProject.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -27,3 +28,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
---
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from src.mlProject.config.configuration import ConfigurationManager
+from src.mlProject.components.data_validation import DataValiadtion
+from src.mlProject import logger
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
